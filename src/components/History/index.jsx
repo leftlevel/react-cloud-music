edd7eabd0f0320d0b6c87e9383a2ad148bbcf13e
Xia, Mao-cong <69415869+leftlevel@users.noreply.github.com>
2021-05-17 16:27:43 +0800

    Add: History 组件

---
+++ src/components/History/index.jsx
@@ -0,0 +1,21 @@
+import React, { memo } from 'react'
+import { withRouter } from 'react-router-dom'
+import Icon from 'components/Icon'
+import './index.scss'
+
+function History(props) {
+  const { history } = props
+
+  const goBack = () => {
+    history.goBack()
+  }
+
+  return (
+    <div className='history-wrapper'>
+      <Icon backdrop={true} type={'back'} click={goBack}/>
+      <Icon backdrop={true} type={'forward'}/>
+    </div>
+  )
+}
+
+export default withRouter(memo(History))
