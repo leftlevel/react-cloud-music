5289c0f4a8d70ce149e420717fc02a0eef40ca5d
Xia, Mao-cong <69415869+leftlevel@users.noreply.github.com>
2021-05-19 18:08:18 +0800

    Add: Search组件

---
+++ src/components/Search/Suggest/index.jsx
@@ -0,0 +1,74 @@
+import React, { Fragment, memo } from 'react'
+import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as musicAction from 'store/music/action'
+import Icon from 'components/Icon'
+import { createSong } from 'utils'
+import './index.scss'
+
+function Suggest(props) {
+  const { items, history, musicAction: { startSong } } = props
+
+  const handleItemClick = (item, childItem) => {
+    if (item.type === 'song') onClickSong(childItem)
+    if (item.type === 'mv') onClickMv(childItem)
+    if (item.type === 'playlist') onClickPlaylist(childItem)
+  }
+
+  const onClickPlaylist = item => {
+    const { id } = item
+    history.push(`/playlist/${id}`)
+  }
+
+  const onClickSong = item => {
+    const { id, name, artists, duration, mvid, album: { id: albumId, name: albumName } } = item
+    const song = createSong({ id, name, artists, duration, albumId, albumName, mvId: mvid })
+    startSong(song)
+  }
+
+  const onClickMv = item => {
+    const { id } = item
+    history.push(`/mv/${id}`)
+  }
+
+  return (
+    <Fragment>
+      {
+        items.map((item, index) => {
+          return (
+            <div className='search-result-wrapper' key={index}>
+              {
+                item.data && item.data.length && (
+                  <div className='result-title'>
+                    <Icon type={item.icon} size={14} />
+                    <span className='song'>{item.title}</span>
+                  </div>
+                )
+              }
+              <ul>
+                {
+                  item.data && item.data.length && item.data.map((childItem, childIndex) => {
+                    return (
+                      <li
+                        className='result-item'
+                        key={childIndex}
+                        onClick={() => handleItemClick(item, childItem)}
+                      >
+                        <span>{item.renderName ? item.renderName(childItem) : childItem.name}</span>
+                      </li>
+                    )
+                  })
+                }
+              </ul>
+            </div>
+          )
+        })
+      }
+    </Fragment>
+  )
+}
+
+const mapDispatchToProps = dispatch => ({ musicAction: bindActionCreators(musicAction, dispatch) })
+
+export default connect(null, mapDispatchToProps)(withRouter(memo(Suggest)))
---
+++ src/components/Search/index.jsx
@@ -0,0 +1,153 @@
+import React, { memo, useState, useRef, useEffect, Fragment, useMemo } from 'react'
+import { findDOMNode } from 'react-dom'
+import { withRouter } from 'react-router-dom'
+import { SearchOutlined } from '@ant-design/icons'
+import { Input } from 'antd'
+import { getSearchHot, getSearchSuggest } from 'api/search'
+import { setStorage, getStorage, SEARCH_HISTORY_KEY, isDef, genArtistisText} from 'utils'
+import useDebounce from 'utils/hooks'
+import Panel from './Panel'
+import Suggest from './Suggest'
+import './index.scss'
+
+function SearchInput(props) {
+  const { history } = props
+  const panelEl = useRef()
+  const inputEl = useRef()
+  const [searchKeyword, setSearchKeyword] = useState('')
+  const [searchHots, setSearchHots] = useState([])
+  const [searchHistorys, setSearchHistory] = useState([])
+  const [searchSuggest, setSearchSuggest] = useState([])
+  const [panelVisible, setPanelVisible] = useState(false)
+  const [inPanel, setInPanel] = useState(false)
+
+  const handleGetSearchSuggest = useDebounce(async value => {
+    if (value) {
+      const { result } = await getSearchSuggest(value)
+      setSearchSuggest(result)
+    }
+  }, 500)
+
+  // 特殊处理搜索建议列表，方便渲染
+  const genSuggestList = useMemo(() => {
+    return [
+      {
+        title: '单曲',
+        icon: 'music',
+        type: 'song',
+        data: searchSuggest.songs,
+        renderName(song) {
+          return `${song.name}-${genArtistisText(song.artists)}`
+        }
+      },
+      {
+        title: '歌单',
+        icon: 'playlist',
+        type: 'playlist',
+        data: searchSuggest.playlists
+      },
+      {
+        title: 'mv',
+        icon: 'mv',
+        data: searchSuggest.mv,
+        type: 'mv',
+        renderName(mv) {
+          return `${mv.name}-${genArtistisText(mv.artists)}`
+        }
+      }
+    ].filter(item => item.data && item.data.length)
+  }, [searchSuggest])
+
+  const handleFocus = () => {
+    setPanelVisible(true)
+    setInPanel(true)
+  }
+
+  const handleChange = e => {
+    const value = e.target.value.replace(/^ +| +$/g, '')
+    setSearchKeyword(value)
+    handleGetSearchSuggest(value)
+  }
+
+  // 按下回车回调
+  const handlePressEnter = e => {
+    const res = searchHistorys.concat([{ first: e.target.value }])
+    setSearchHistory(res)
+    setStorage(SEARCH_HISTORY_KEY, res)
+    goSearch(e.target.value)
+  }
+
+  const clickTag = word => {
+    goSearch(word)
+  }
+
+  const goSearch = async value => {
+    if (!value) return
+    history.push(`/search/${value}/songs`)
+  }
+
+  const clickEvent = e => {
+    /* 
+    能获取到 panel 这个 dom 节点，同时鼠标点击的位置不包含在这个区域内
+    且鼠标不是在搜索栏的输入框内的时候，将 panel 的显示状态设置为 false
+    */
+    if (panelEl.current && !panelEl.current.contains(e.target) && !findDOMNode(inputEl.current).contains(e.target)) {
+      setInPanel(false)
+      return
+    }
+  }
+
+  const getSearchHotList = async () => {
+    const { result: { hots }} = await getSearchHot()
+    setSearchHots(hots)
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', clickEvent)
+    return () => {
+      document.removeEventListener('mousedown', clickEvent)
+    }
+  }, [])
+
+  useEffect(() => {
+    // 从本地读取搜索历史
+    const searchHistory = getStorage(SEARCH_HISTORY_KEY)
+    if (isDef(searchHistory) && searchHistory.length) {
+      setSearchHistory(searchHistory)
+    }
+    getSearchHotList()
+  }, [])
+
+  return (
+    <div className='search-wrapper'>
+      <Input 
+        onFocus={handleFocus}
+        onPressEnter={handlePressEnter}
+        value={searchKeyword}
+        ref={inputEl}
+        placeholder='搜索看看'
+        onChange={handleChange}
+        prefix={<SearchOutlined />}
+      />
+      {
+        panelVisible && inPanel && (
+          <div className='search-panel-wrapper' ref={panelEl}>
+            {/* 有搜索关键字及搜索出来的数据中有一项有 data 这个数据，就需要展示到检索面板 */}
+            {
+              searchKeyword && genSuggestList.some(suggest => suggest.data) ? (
+                <Suggest items={genSuggestList} />
+              ) : (
+                <Fragment>
+                  <Panel items={searchHots} title={'热门搜索'} clickTag={clickTag}/>
+                  <Panel items={searchHistorys} title={'搜索历史'} clickTag={clickTag}/>
+                </Fragment>
+              )
+            }
+          </div>
+        )
+      }
+    </div>
+  )
+}
+
+export default withRouter(memo(SearchInput))
