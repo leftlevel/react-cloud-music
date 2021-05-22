99c344e58778f49d6c0986f0a40752d48d219e90
Xia, Mao-cong <69415869+leftlevel@users.noreply.github.com>
2021-05-22 15:38:34 +0800

    Add: Discovery 下的 Banner组件

---
+++ src/views/Discovery/components/Banner/index.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect, memo } from 'react'
+import { Carousel } from 'antd'
+import { getBanner } from 'api/discovery'
+import './index.scss'
+
+function Banner() {
+  const [bannerList, setBannerList] = useState([])
+  const getBannerList = async () => {
+    const { banners: banner } = await getBanner(0)
+    console.log(banner)
+    setBannerList(banner)
+  }
+
+  useEffect(() => {
+    getBannerList()
+  }, [])
+
+  return (
+    <Carousel autoplay className='banner-carousel'>
+      {
+        bannerList.length && bannerList.map((banner) => {
+          return (
+            <img
+              className='banner-img'
+              src={banner.imageUrl}
+              key={banner.targetId}
+              alt=""
+            />
+          )
+        })
+      }
+    </Carousel>
+  )
+}
+
+export default memo(Banner)
---
+++ src/views/Discovery/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Banner } from './components'
+import './index.scss'
+
+function Discovery() {
+  return (
+    <div className='discovery-wrapper'>
+      <Banner />
+    </div>
+  )
+}
+
+export default Discovery
