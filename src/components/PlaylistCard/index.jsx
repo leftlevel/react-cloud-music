46496aea0a652785b76c320022b2e00b25f6978f
Xia, Mao-cong <69415869+leftlevel@users.noreply.github.com>
2021-05-23 11:36:17 +0800

    Add: 推荐歌单播放卡片组件

---
+++ src/components/PlaylistCard/index.jsx
@@ -0,0 +1,40 @@
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+import LazyLoad from 'react-lazyload'
+import { withRouter } from 'react-router-dom'
+import PlayIcon from 'components/PlayIcon'
+import './index.scss'
+
+function PlaylistCard(props) {
+  const { picUrl, copywriter, id, name, history } = props
+
+  const handleClickCard = () => {
+    history.push(`/playlist/${id}`)
+  }
+
+  return (
+    <div className='playlist-card' onClick={handleClickCard}>
+      <div className='card-img'>
+        <LazyLoad overflow={true} offset={200}>
+          <img src={picUrl} alt="" />
+        </LazyLoad>
+        <div className='card-desc'>
+          <span className='desc'>{copywriter}</span>
+        </div>
+        <div className='card-icon'>
+          <PlayIcon size={36} />
+        </div>
+      </div>
+      <div className='card-name'>{name}</div>
+    </div>
+  )
+}
+
+PlaylistCard.propTypes = {
+  picUrl: PropTypes.string,
+  copywriter: PropTypes.string,
+  id: PropTypes.number,
+  name: PropTypes.string
+}
+
+export default withRouter(memo(PlaylistCard))
