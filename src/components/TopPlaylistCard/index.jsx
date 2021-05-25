5bc676d08df8d55ef0534470c0963c2d238d037f
Xia, Mao-cong <69415869+leftlevel@users.noreply.github.com>
2021-05-25 11:08:24 +0800

    Add: 顶部精品推荐播放列表卡片

---
+++ src/components/TopPlaylistCard/index.jsx
@@ -0,0 +1,40 @@
+import React, { memo } from 'react'
+import { withRouter } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import './index.scss'
+
+function TopPlaylistCard(props) {
+  const { id, coverImgUrl, name, description, history } = props
+  
+  const handleClickCard = () => {
+    history.push(`/playlist/${id}`)
+  }
+
+  return (
+    <div className='top-card-wrapper' onClick={handleClickCard}>
+      <div className='main'>
+        <div className='img-wrap'>
+          <img src={coverImgUrl} alt="" />
+        </div>
+        <div className='content-wrap'>
+          <div className='tag'>
+            <span>精品歌单</span>
+          </div>
+          <p className='name'>{name}</p>
+          <p className='desc'>{description}</p>
+        </div>
+      </div>
+      <div className='background' style={{ backgroundImage: `url(${coverImgUrl})`}}></div>
+      <div className='background-mask'></div>
+    </div>
+  )
+}
+
+TopPlaylistCard.propTypes = {
+  id: PropTypes.number,
+  coverImgUrl: PropTypes.string,
+  name: PropTypes.string,
+  description: PropTypes.string
+}
+
+export default withRouter(memo(TopPlaylistCard))
