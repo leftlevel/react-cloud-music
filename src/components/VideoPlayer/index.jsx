9c99c0bec5b6a280552078c798d8d6d52f5ed6f9
Xia, Mao-cong <1025277736@qq.com>
2021-06-03 10:06:42 +0800

    Add: VideoPlayer 组件

---
+++ src/components/VideoPlayer/index.jsx
@@ -0,0 +1,50 @@
+import React, { memo, useState, useEffect, useRef, forwardRef } from 'react'
+import Player from 'xgplayer'
+
+const VideoPlayer = forwardRef((props, ref) => {
+  const { url, poster } = props
+  const player = useRef(ref)
+
+  const [playerCor, setPlayerCor] = useState(null)
+
+  const initPlayer = () => {
+    if (!url) return
+    const playerCor = new Player({
+      el: player.current,
+      // 容器宽度
+      width: '100%',
+      // 初始化显示视频首帧
+      videoInit: true,
+      // 播放源
+      url,
+      // 语言
+      lang: 'zh-cn',
+      // 初始音量
+      volume: 0.8,
+      // 封面
+      poster: poster,
+      // 自动播放
+      autoplay: false,
+      // 播放倍速
+      playbackRate: [0.5, 0.75, 1, 1.5, 2]
+    })
+
+    setPlayerCor(playerCor)
+  }
+
+  useEffect(() => {
+    initPlayer()
+  }, [url])
+
+  useEffect(() => {
+    // if (playerCor) {
+    //   console.log(playerCor)
+    // }
+  }, [playerCor])
+
+  return (
+    <div ref={player}></div>
+  )
+})
+
+export default memo(VideoPlayer)
