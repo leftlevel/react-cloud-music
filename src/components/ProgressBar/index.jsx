b36aa49c456225c5ea8e6623fbeb6e49e463c6de
Xia, Mao-cong <1025277736@qq.com>
2021-06-10 10:26:56 +0800

    Add: ProgressBar 组件

---
+++ src/components/ProgressBar/index.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef } from 'react'
+import PropTypes from 'prop-types'
+import './index.scss'
+
+function ProgressBar(props) {
+  const { percent, progressInput, progressChange, step } = props
+
+  const progress = useRef(null)
+  
+  const onProgressChange = e => {
+    e.persist()
+    progressChange(e.target.value)
+  }
+  const onProgressInput = e => {
+    e.persist()
+    progressInput(e.target.value)
+  }
+  const updateProgressBg = e => {
+    progress.current.style.backgroundSize = `${percent}% 100%`
+  }
+
+  useEffect(() => {
+    updateProgressBg()
+  }, [percent])
+
+  return (
+    <input
+      className='progress'
+      type='range'
+      max='100'
+      min='0'
+      step={step}
+      value={percent}
+      onChange={onProgressChange}
+      onInput={onProgressInput}
+      ref={progress}
+    />
+  )
+}
+
+ProgressBar.defaultProps = {
+  percent: 0,
+  step: '1'
+}
+
+ProgressBar.propTypes = {
+  percent: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  progressChange: PropTypes.func,
+  progressInput: PropTypes.func,
+  step: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+}
+export default ProgressBar
